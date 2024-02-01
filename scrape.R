1bbd14d5e272d1aff639da1ffaffd2f26060f8a1
jokoding.com <109510091+jokoadenur@users.noreply.github.com>
2024-02-01 19:44:49 +0700

    Update scrape.R

--- scrape.R
+++ scrape.R
@@ -4,20 +4,19 @@ rm(list=ls())
 # libraries
 library(dplyr)
 library(rvest)
+library(stringr)
+library(openxlsx)
 
 # url awal
 url = "https://www.detik.com/"
 
 # ambil headline
 headliner = 
-  url %>% 
-  read_html() %>% 
-  html_nodes(".media__link") %>% 
-  html_text(trim = T)
+  url %>% read_html() %>% html_nodes(".media__link") %>% html_text() %>% str_squish()
 
 # buat nama file
 nama_file = Sys.time() %>% as.character()
-nama_file = paste0(nama_file,".csv")
+nama_file = paste0(nama_file,".xlsx")
 
 # save headliner
-write.csv(headliner,nama_file)
+write.xlsx(headliner,nama_file)
