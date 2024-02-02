98a5f9ca6659806fdfe097f442fdd42382ee86bc
jokoding.com <109510091+jokoadenur@users.noreply.github.com>
2024-02-03 05:58:44 +0700

    Update scrape.R

--- scrape.R
+++ scrape.R
@@ -7,16 +7,31 @@ library(rvest)
 library(stringr)
 library(openxlsx)
 
-# url awal
-url = "https://www.detik.com/"
+dptisi <- function(x){
+  isi <- read_html(x)
+  isiku <- isi %>% html_nodes('.itp_bodycontent h2 , p') %>% html_text() %>% paste(collapse = " ")
+  return(isiku)
+}
 
-# ambil headline
-headliner = 
-  url %>% read_html() %>% html_nodes(".media__link") %>% html_text() %>% str_squish()
+detikjatim <- data.frame()
+
+for(hasil in seq(from = 1, to = 2, by = 1)){
+  url <-paste0("https://www.detik.com/search/searchall?query=Jatim&siteid=2&sortby=time&page=", hasil)
+  laman <- read_html(url)
+ 
+  judul <- laman %>% html_nodes('span .title') %>% html_text()
+  tgl <- laman %>% html_nodes('span .date') %>% html_text()
+  link_judul <- laman %>% html_nodes('.list-berita a') %>% html_attr("href")
+ 
+  isiberita <- sapply(link_judul, FUN = dptisi, USE.NAMES = F)
+ 
+  detikjatim <- rbind(detikjatim, data.frame(judul, tgl, isiberita, link_judul, stringsAsFactors = F))
+  print(paste("page ke-", hasil))
+}
 
 # buat nama file
 nama_file = Sys.time() %>% as.character()
 nama_file = paste0(nama_file,".xlsx")
 
 # save headliner
-write.xlsx(headliner,nama_file)
+write.xlsx(detikjatim,nama_file)
